fd2075a03c517039d419381222b26f11ae2d36d2
morenonavarroluis <morenoluismanu@gmail.com>
2026-08-20 14:27:28 -0400

    agregar modificar configuracion

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-x1e7%1ug@jy0_ks77%jcu=_u_u=w-_x04v$o=$(phdss!rt#ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newlevel.pythonanywhere.com']
 
 
 # Application definition
@@ -76,16 +76,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'facturacion_web',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'facturacion_web',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
