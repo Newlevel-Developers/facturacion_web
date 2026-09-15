6a1b37ac956773fdaf5fec42484c7e0211cc7dce
morenonavarroluis <morenoluismanu@gmail.com>
2026-09-15 15:39:26 -0400

    Agregar  merge

--- config/settings.py
+++ config/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'facturacion_web',
-        'USER': 'postgres',
-        'PASSWORD': 'mercaldev2026',
-        'HOST': '10.30.22.231',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'facturacion_web',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mercaldev2026',
+#         'HOST': '10.30.22.231',
+#         'PORT': '5432',
+#     }
+# }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
 
 
 
